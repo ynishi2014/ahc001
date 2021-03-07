44ef21d5000ff8a09ee874e166664fc5efbaccec
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2021-03-07 11:10:57 +0900

    縦横両方のパターンを試し良い方を出力する　2021-03-06 19:58:20
    点数計算を実装
    処理を関数化

--- solver.php
+++ solver.php
@@ -2,43 +2,67 @@
 [$N] = ints();
 for($i = 0; $i < $N; $i++){
     [$x,$y,$r] = ints();
-    $map[$y][$x] = $r;
     $X[] = $x; $Y[] = $y; $R[] = $r; $I[] = $i;
 }
-$origR = $R;
-$origX = $X;
-array_multisort($R, $X, $Y, $I);
-
-for($i = 0; $i < $N; $i++){
-  $yt = $Y[$i]; $yb = $Y[$i]+1;
-  $xr = $X[$i]; $xl = $X[$i];
-  $r = $R[$i];
-  while(!isset($map[$yt][$xr+1]) && $xr < 10000 && $xr - $X[$i] < $r){
-    $xr++;
-  }
-  while(count($map[$yt]) == 1 && !isset($map[$yt][$xl-1]) && $xl > 0 && $xr - $xl < $r){
-    $xl--;
-  }
-  while(!isset($map[$yt-1]) && $yt > 0 && ($yb-$yt)*($xr-$xl) < $r){
-    $yt--;
-    $map[$yt] = true;
-  }
-  while(!isset($map[$yb+1]) && !isset($map[$yb]) && $yb < 10000 && ($yb-$yt)*($xr-$xl) < $r){
-    $yb++;
-    $map[$yb] = true;
-  }
-  $result[$I[$i]] = [$xl,$yt,$xr,$yb];
-}
-for($i = 0; $i < $N; $i++){
-    while(($result[$i][3]-$result[$i][1])*($result[$i][2]-$result[$i][0]) > $origR[$i] && $result[$i][0] < $origX[$i]){
-        $result[$i][0]++;
+[$result1, $score1] = solve($X,$Y,$R,$I);
+[$result2, $score2] = solve($Y,$X,$R,$I);
+$flip = $score1 < $score2;
+if($flip){
+    $result = $result2;
+    for($i = 0; $i < $N; $i++){
+        [$result[$i][0],$result[$i][1],$result[$i][2],$result[$i][3]] = [$result[$i][1],$result[$i][0],$result[$i][3],$result[$i][2]]; 
     }
-    while(($result[$i][3]-$result[$i][1])*($result[$i][2]-$result[$i][0]) > $origR[$i] && $result[$i][2] > $origX[$i]){
-        $result[$i][2]--;
-    }
+}else{
+    $result = $result1;
 }
 for($i = 0; $i < $N; $i++){
     echo implode(" ", $result[$i]),"\n";
+}
+function solve($X,$Y,$R,$I){
+    $N = count($X);
+    for($i = 0; $i < $N; $i++){
+        $map[$Y[$i]][$X[$i]] = 1;
+    }
+    $origR = $R;
+    $origX = $X;
+    array_multisort($R, $X, $Y, $I);
+
+    for($i = 0; $i < $N; $i++){
+    $yt = $Y[$i]; $yb = $Y[$i]+1;
+    $xr = $X[$i]; $xl = $X[$i];
+    $r = $R[$i];
+    while(!isset($map[$yt][$xr+1]) && $xr < 10000 && $xr - $X[$i] < $r){ // 右に伸ばす
+        $xr++;
+    }
+    while(count($map[$yt]) == 1 && !isset($map[$yt][$xl-1]) && $xl > 0 && $xr - $xl < $r){ // 左に伸ばす
+        $xl--;
+    }
+    while(!isset($map[$yt-1]) && $yt > 0 && ($yb-$yt)*($xr-$xl) < $r){ // 上に伸ばす
+        $yt--;
+        $map[$yt] = true;
+    }
+    while(!isset($map[$yb+1]) && !isset($map[$yb]) && $yb < 10000 && ($yb-$yt)*($xr-$xl) < $r){ // 下に伸ばす
+        $yb++;
+        $map[$yb] = true;
+    }
+    $result[$I[$i]] = [$xl,$yt,$xr,$yb];
+    }
+    for($i = 0; $i < $N; $i++){
+        while(($result[$i][3]-$result[$i][1])*($result[$i][2]-$result[$i][0]) > $origR[$i] && $result[$i][0] < $origX[$i]){
+            $result[$i][0]++; // 左を縮める
+        }
+        while(($result[$i][3]-$result[$i][1])*($result[$i][2]-$result[$i][0]) > $origR[$i] && $result[$i][2] > $origX[$i]){
+            $result[$i][2]--; // 右を縮める
+        }
+    }
+    $sumP = 0;
+    for($i = 0; $i < $N; $i++){
+        $s = ($result[$i][3]-$result[$i][1])*($result[$i][2]-$result[$i][0]);
+        $p = 1-(1-min($origR[$i], $s)/max($origR[$i], $s))**2;
+        $sumP += $p;
+    }
+    $sumP/=$N;
+    return [$result, $sumP];
 }
 
 function ints($n = false){
