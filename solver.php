ff16681211ddfb7c43f47d93033e0bc10a67734d
Yoichiro Nishimura <y.nishimura@tatsuno-system.co.jp>
2021-03-07 11:06:30 +0900

    上下に伸ばすときの境界値を微調整（幅の0.5倍以上あるか確認　2021-03-06 17:17:56

--- solver.php
+++ solver.php
@@ -15,28 +15,22 @@ for($i = 0; $i < $N; $i++){
   while(count($map[$yt]) == 1 && !isset($map[$yt][$xl-1]) && $xl > 0 && $xr - $xl < $r){
     $xl--;
   }
-  while(!isset($map[$yt-1]) && $yt > 0 && ($yb-$yt)*($xr-$xl) < $r){
+  while(!isset($map[$yt-1]) && $yt > 0 && ($yb-$yt + 0.5)*($xr-$xl) < $r){
     $yt--;
     $map[$yt] = true;
   }
-  while(!isset($map[$yb+1]) && !isset($map[$yb]) && $yb < 10000 && ($yb-$yt)*($xr-$xl) < $r){
+  while(!isset($map[$yb+1]) && !isset($map[$yb]) && $yb < 10000 && ($yb-$yt+0.5)*($xr-$xl) < $r){
     $yb++;
     $map[$yb] = true;
   }
   echo implode(" ", [$xl,$yt,$xr,$yb]),"\n";
 }
 
-
-function str(){return trim(fgets(STDIN));}
 function ints($n = false){
-  if($n===false){
-    return array_map('intval',explode(' ',trim(fgets(STDIN))));
-  }else{$ret = [];for($i = 0; $i < $n; $i++)foreach(array_map('intval',explode(' ',trim(fgets(STDIN)))) as $j => $v)$ret[$j][] = $v;return $ret;}
+   return array_map('intval',explode(' ',trim(fgets(STDIN))));
 }
-function int(){return intval(trim(fgets(STDIN)));}
 function chmax(&$a,$b){if($a<$b){$a=$b;return 1;}return 0;}
 function chmin(&$a,$b){if($a>$b){$a=$b;return 1;}return 0;}
-function popcount($x){$c=0;while($x){$x&=$x-1;++$c;}return$c;}
 function o(...$val){
   if(count($val)==1)$val=array_shift($val);
   echo debug_backtrace()[0]['line'].")";
@@ -52,44 +46,3 @@ function o(...$val){
 }
 function addIndex($val){if(!isCleanArray($val)){$val = array_map(function($k, $v){return $k.":".$v;}, array_keys($val), $val);}return $val;}
 function isCleanArray($array){$clean=true;$i = 0;foreach($array as $k=>$v){if($k != $i++)$clean = false;}return $clean;}
-// 座圧対象の配列 -> [圧縮された配列,復元用のMap,圧縮用のMap]
-function atsu($array){$a = array_flip($array);$fuku=array_flip($a);sort($fuku);$atsu = array_flip($fuku);foreach($array as $i=>$val)$array[$i]=$atsu[$val];return [$array, $fuku, $atsu];}
-function median($a){sort($a);$n=count($a);return $n%2?$a[($n-1)/2]:($a[$n/2-1]+$a[$n/2])/2;}
-function gcdAll($array){$gcd=$array[0];for($i=1,$I=count($array);$i<$I;++$i){$gcd=gcd($gcd,$array[$i]);}return $gcd;}
-function gcd($m, $n){if(!$n)return $m;return gcd($n, $m % $n);}
-function lcmAll($array){$lcm=$array[0];for($i=1,$I=count($array);$i<$I;++$i){$lcm=lcm($lcm,$array[$i]);}return $lcm;}
-function lcm($a, $b) {return $a / gcd($a, $b) * $b;}
-function loadTree($N = false){
-  if($N === false)$N = $GLOBALS['N'];
-  return loadGraph($N, $N-1);
-}
-function loadGraph($N = false, $M = false, $both = true){
-  if($N === false)$N = $GLOBALS['N'];
-  if($M === false)$M = $GLOBALS['M'];
-  $G = array_fill(1, $N, []);
-  for($i = 0; $i < $M; $i++){
-    $values = ints();
-    if(count($values) == 2){
-      list($a, $b) = $values;
-      if($both == 0){
-        $G[$a][] = $b;
-      }elseif($both == 1){
-        $G[$a][] = $b;
-        $G[$b][] = $a;
-      }else{
-        $G[$b][] = $a;
-      }
-    }else{
-      list($a, $b, $d) = $values;
-      if($both == 0){
-        $G[$a][] = [$b, $d];
-      }elseif($both == 1){
-        $G[$a][] = [$b, $d];
-        $G[$b][] = [$a, $d];
-      }else{
-        $G[$b][] = [$a, $d];
-      }
-    }
-  }
-  return $G;
-}
